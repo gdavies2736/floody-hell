dabd0c65063edd7c8864f1b0234a4d45d9956cd5
Ha Phan <ha.markus.phan@gmail.com>
2024-01-18 21:32:53 +0000

    try map element

--- src/components/PolygonMap.jsx
+++ src/components/PolygonMap.jsx
@@ -6,30 +6,52 @@ const Map = (props) => {
   const map = useRef(null);
   const platform = useRef(null);
   const { apikey } = props;
-}
 
-function PolygonMap () {
-  const platform = new H.service.Platform({
-    'apikey': 'geNXYHrS1agADyZRRPuK8Ye2x1YJcZIbWIbyhBtcdUs' // API key from platform.here.com
-  });
-  // Obtain the default map types from the platform object:
-  const defaultLayers = platform.createDefaultLayers();
-
-  // Instantiate (and display) a map object:
-  const map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-      zoom: 10,
-      center: { lat: 52.5, lng: 13.4 }
-    });
+  useEffect(
+    () => {
+      // Check if the map object has already been created
+      if (!map.current) {
+        // Create a platform object with the API key
+        platform.current = new H.service.Platform({ apikey });
+        // Create a new Raster Tile service instance
+        const rasterTileService = platform.current.getRasterTileService({
+          queryParams: {
+            style: "explore.day",
+            size: 512,
+          },
+        });
+        // Creates a new instance of the H.service.rasterTile.Provider class
+        // The class provides raster tiles for a given tile layer ID and pixel format
+        const rasterTileProvider = new H.service.rasterTile.Provider(
+          rasterTileService
+        );
+        // Create a new Tile layer with the Raster Tile provider
+        const rasterTileLayer = new H.map.layer.TileLayer(rasterTileProvider);
+        // Create a new map instance with the Tile layer, center and zoom level
+        const newMap = new H.Map(mapRef.current, rasterTileLayer, {
+          pixelRatio: window.devicePixelRatio,
+          center: {
+            lat: 64.144,
+            lng: -21.94,
+          },
+          zoom: 14,
+        });
   
-  return(
-        <>
-          <div style="width: 640px; height: 480px" id="mapContainer">
-            
-          </div>
-        </>)
+        // Add panning and zooming behavior to the map
+        const behavior = new H.mapevents.Behavior(
+          new H.mapevents.MapEvents(newMap)
+        );
+  
+        // Set the map object to the reference
+        map.current = newMap;
+      }
+    },
+    // Dependencies array
+    [apikey]
+  );
+
+// Return a div element to hold the map
+return <div style={ { width: "100%", height: "500px" } } ref={mapRef} />;
 }
 
-export default PolygonMap;
+export default Map;
