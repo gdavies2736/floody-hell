7acddc94f7ae25c4f4aa6a2599ad671348676049
Rachael Potter <rachaelpotter32@gmail.com>
2024-01-23 18:24:10 +0000

    added header and created branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "../styles/Header.css";
+import image from '../images/riverimage.jpg'
 
 function Header () {
     return (
-        <>
-        <p>Test for Header</p>
-        </>
-    )
-}
+    <header>
+            <div className="opacity-100 h-60 w-full mx-auto text-center bg-center bg-cover  bg-no-repeat bg-[url('..\src\images\riverimage.jpg')]">
+                <h1 className="pt-5 text-white text-7xl text-left indent-8">Floody-Hell</h1>
+                <p className="indent-5 text-white font-semibold text-2xl text-left ">Find up to date flood information plotted onto a polygon map using our postcode search function.</p>
+            </div>
+     </header> )
+};
 
 export default Header;
