0f983e54d2ff11112f9f4b2b4b7a2bbb9b958594
Rachael Potter <rachaelpotter32@gmail.com>
2024-01-23 19:50:24 +0000

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import "../styles/Header.css";
 
 function Header () {
     return (
-        <>
-        <p>Test for Header</p>
-        </>
+        <div className="opacity-100 h-60 w-full mx-auto text-center bg-center bg-cover  bg-no-repeat bg-[url('..\src\images\riverimage.jpg')]">
+                <h1 className="pt-5 text-white text-7xl text-left indent-8">Floody-Hell</h1>
+                <p className="indent-5 text-white font-semibold text-2xl text-left ">Find up to date flood information plotted onto a polygon map using our postcode search function.</p>
+        </div>
     )
 }
 
