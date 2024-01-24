033709284bd0d1850759e54fe3a210a8a9909e05
Ha Phan <ha.markus.phan@gmail.com>
2024-01-24 20:25:30 +0000

    remove the if statement in Maptest

--- src/components/Maptest.jsx
+++ src/components/Maptest.jsx
@@ -5,24 +5,25 @@ import PolygonObject from "./PolygonObject";
 import "../styles/PolygonMap.css";
 
 function Maptest() {
-    const mapRef = useRef(null);
-    const map = useRef(null);
-    const platform = useRef(null);
+    const mapArray = []; // To hold all the possible maps that can be created
     const { apikey, testPolygonObject } = polygonmapAPI;
-
-    let latAverage = 0;
-    let lngAverage = 0;
-    let count = 0;
-
-    testPolygonObject.features[0].geometry.coordinates[0].map(elem => {
-        latAverage += elem[1];
-        lngAverage += elem[0];
-        count ++;
-    })
     
     useEffect(() => {
-        if (!map.current){
-            platform.current = new H.service.Platform({ apikey });
+        const mapRef = useRef(null);
+        const map = useRef(null);
+        const platform = useRef(null);
+    
+        let latAverage = 0;
+        let lngAverage = 0;
+        let count = 0;
+
+        testPolygonObject.features[0].geometry.coordinates[0].map(elem => {
+            latAverage += elem[1];
+            lngAverage += elem[0];
+            count ++;
+        })
+        
+        platform.current = new H.service.Platform({ apikey });
             const defaultLayers = platform.current.createDefaultLayers(); // It is platform.current.createDefaultLayers(), not platform.createDefaultLayers()
 
             const newMap = new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
@@ -53,7 +54,9 @@ function Maptest() {
             ui.addControl("overview", overviewMap);
 
             map.current = newMap;
-        }
+            mapArray.push(map);
+        // if (!map.current){
+        // }
     }, [apikey])
     // window.addEventListener("resize", () => map.getViewPort().resize()); //change to the one in return
     
