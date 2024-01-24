675f981f68f60f799e19304d0597d6ad3b08f511
Ha Phan <ha.markus.phan@gmail.com>
2024-01-24 23:21:38 +0000

    test with async await

--- src/components/ObjectTest.jsx
+++ src/components/ObjectTest.jsx
@@ -5,17 +5,16 @@ import polygonmapAPI from "../utils/polygonmapAPI";
 function PolygonObjectTest () {
     const polygonArr = []; // To hold all polygon arrays
     const lineStringArr = []; // To hold all linestring object from the polygon Arr
-    
-    const BaseURL = `https://environment.data.gov.uk/flood-monitoring/id/floods?lat=54.28219&long=-0.38364&dist=30`;
-    
-    fetch(BaseURL)
-    .then((response) => {return response.json()})
-    .then((result) => {
+    // const BaseURL = `https://environment.data.gov.uk/flood-monitoring/id/floods?lat=54.28219&long=-0.38364&dist=30`;
+    const baseURLFetch = async () => {
+        const BaseURL = `https://environment.data.gov.uk/flood-monitoring/id/floods?lat=54.28219&long=-0.38364&dist=30`;
+        const response = await fetch(BaseURL);
+        const result = await response.json();
         result.items.map((elem) => {
-            // console.log("Elem: ", elem);
             fetch(elem.floodArea.polygon)
             .then((polygon) => {return polygon.json()})
             .then((data) => {
+                // console.log("Data", data.features[0].geometry.coordinates[0]);
                 let coordinate = [];
                 data.features[0].geometry.coordinates[0][0].map(elem1 => {
                     // console.log("Elem1: ", elem1);
@@ -26,7 +25,27 @@ function PolygonObjectTest () {
                 polygonArr.push(coordinate); // After finishing for each elem object, a new array is added into the polygonArr array.
             })
         })
-    })
+    }
+    baseURLFetch();
+    // fetch(BaseURL)
+    // .then((response) => {return response.json()})
+    // .then((result) => {
+    //     result.items.map((elem) => {
+    //         // console.log("Elem: ", elem);
+    //         fetch(elem.floodArea.polygon)
+    //         .then((polygon) => {return polygon.json()})
+    //         .then((data) => {
+    //             let coordinate = [];
+    //             data.features[0].geometry.coordinates[0][0].map(elem1 => {
+    //                 // console.log("Elem1: ", elem1);
+    //                 coordinate.push(elem1[1]);
+    //                 coordinate.push(elem1[0]);
+    //                 coordinate.push(100);
+    //             })
+    //             polygonArr.push(coordinate); // After finishing for each elem object, a new array is added into the polygonArr array.
+    //         })
+    //     })
+    // })
 
     console.log("PolygonArr: ", polygonArr);
     polygonArr.map((elem) => {
@@ -36,7 +55,7 @@ function PolygonObjectTest () {
         lineStringArr.push(lineString);
     });
     
-    return ({lineStringArr});
+    return (<></>);
 }
 
 export default PolygonObjectTest;
